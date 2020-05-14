3f56ec340cae3e3286572580a2c21127b5e299d3
Harshilbhardwaj47 <edwardkenway200017@gmail.com>
2020-05-13 20:31:10 -0700

    student class minor changes and createID method added

--- SRC/Student.java
+++ SRC/Student.java
@@ -2,21 +2,30 @@ package studataapp;
 
 public class Student{
 
-	private String Firstname;
-	private String lastname;
+	private String name;
 	private int year;
-	private int ID;
+	private String StudentID;
 	private int balance;
+	private static int ID=0;
 
 	public Student() {
 		Scanner in = new Scanner(System.in);
-		System.out.println("Enter your first name: ");
-		Firstname = in.next();
-		System.out.println("Enter your last name: ");
-		lastname = in.next();
+		// Input Full Name
+		System.out.println("Enter your full name name: ");
+		name = in.nextLine();
+		//Input in which Year a Student is
 		System.out.println("Choose your year");
 		System.out.println("1) First year\n2)Second year\n3)Third year\n4)Fourth year");
 		year = in.nextInt();
+		// method create ID called
+		createID();
+		// printing the information taken and genrated
+		System.out.println("Name: " + name + "\nYear: " + year + "\nStudentID: " + StudentID);
 	}
+        private void createID() {
+                  // always must starts from tear in which a student is 
+		  ID++;
+		  StudentID = year + "000" + ID;
+         }   
 }
 
